cf00b0f0ebaa036f8bee58690ed9b5c51d43e0d3
Jimmy Li <jmyli562@gmail.com>
2023-08-15 12:19:46 -0700

    add user table to database

--- lib/models.py
+++ lib/models.py
@@ -1,6 +1,22 @@
 from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
 engine = create_engine("sqlite:///password_manager.db")
 
 Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String, unique=True, nullable=False)
+    password = Column(String, nullable=False)
+    email = Column(String, nullable=False)
+
+    def __repr__(self):
+        return f"User {self.id} " \
+        + f"{self.username}, " \
+        + f"{self.password}, " \
+        + f"{self.email} "

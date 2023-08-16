6cc62ce839004ce88af129a7d4d84312f6d8e381
Jimmy Li <jmyli562@gmail.com>
2023-08-16 15:01:19 -0700

    add one to many relationship between user and category

--- lib/models.py
+++ lib/models.py
@@ -7,16 +7,22 @@ engine = create_engine("sqlite:///password_manager.db")
 
 Base = declarative_base()
 
+
 class User(Base):
-    __tablename__ = 'users'
+    __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
     username = Column(String, unique=True, nullable=False)
     password = Column(String, nullable=False)
     email = Column(String, nullable=False)
 
+    categories = relationship("Category", back_populates="user")
+    entries = relationship("Entry", back_populates="user")
+
     def __repr__(self):
-        return f"User {self.id} " \
-        + f"{self.username}, " \
-        + f"{self.password}, " \
-        + f"{self.email} "
+        return (
+            f"User {self.id} "
+            + f"{self.username}, "
+            + f"{self.password}, "
+            + f"{self.email} "
+        )

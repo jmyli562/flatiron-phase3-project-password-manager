2e15cd1ee9331fe909d0ab2b9ec7aa30c43f1bb2
Jimmy Li <jmyli562@gmail.com>
2023-08-18 14:01:38 -0700

    add view passwords method

--- lib/cli.py
+++ lib/cli.py
@@ -28,19 +28,67 @@ def add_user():
     password = input("Please enter your password: ")
 
     user = User(username=username, password=password, email=email)
+    session.add(user)
+    session.commit()
 
     print(f"User {username} added successfully!")
 
 
+def view_passwords(user):
+    # retrieving all of the entries that match the filter
+    user_entry = session.query(Entry).filter_by(user_id=user.id).all()
+
+    # no saved passwords were found
+    if not user_entry:
+        print(f"User {user.username} has no saved passwords.")
+        return
+    print("\nSaved Passwords:")
+    for index, entry in enumerate(user_entry, start=1):
+        print(f"\nEntry {index}:")
+        print(f"Website: {entry.website}")
+        print(f"Username: {entry.username}")
+        print(f"Password: {entry.password}")
+        print(f"Category: {entry.category.name}")
+        print("-" * 30)
+    print()
+
+
+def create_entry(user):
+    website = input("Please enter the website url: ")
+    username = input("Please enter your username for that website: ")
+    password = input("Please enter the password: ")
+    note = input("Please type out a note for this website: ")
+
+
 def user_menu(user):
     user_menu = TerminalMenu(
-        ["View Passwords", "Create New Entry", "Update Password", "Logout"]
+        [
+            "View Passwords",
+            "Create New Entry",
+            "Update Password",
+            "View Categories",
+            "Add Tag to Entry",
+            "Search Entry by Tag",
+            "Logout",
+        ]
     )
     while True:
         user_selection = user_menu.show()
-        if user_selection == 3:
+        if user_selection == 0:
+            view_passwords(user)
+        elif user_selection == 1:
+            create_entry(user)
+        elif user_selection == 2:
+            pass
+        elif user_selection == 3:
+            pass
+        elif user_selection == 4:
+            pass
+        elif user_selection == 5:
+            pass
+        elif user_selection == 6:
             print("Logging out...")
-            print(f"User {user.username} has successfully logged out.")
+            print(f"User {user.username} has been successfully logged out.")
             break
 
 

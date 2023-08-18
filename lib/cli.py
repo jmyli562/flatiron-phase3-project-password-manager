a897809317634c90f34a8d594074d9c6a46c808c
Jimmy Li <jmyli562@gmail.com>
2023-08-17 18:14:08 -0700

    added login menu to cli

--- lib/cli.py
+++ lib/cli.py
@@ -1,4 +1,4 @@
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, make_transient
 from models import User, Category, Entry, Tag, engine
 from simple_term_menu import TerminalMenu
 
@@ -8,7 +8,18 @@ session = Session()
 
 
 def login():
-    pass
+    username = input("Please enter your username: ")
+    password = input("Please enter your password: ")
+
+    user = session.query(User).filter_by(username=username).first()
+
+    if user and user.password == password:
+        make_transient(user)
+        print("Login successful!")
+        user_menu(user)
+    else:
+        print("Invalid credentials. Please try again")
+        login()
 
 
 def add_user():
@@ -19,6 +30,18 @@ def add_user():
     user = User(username=username, password=password, email=email)
 
     print(f"User {username} added successfully!")
+
+
+def user_menu(user):
+    user_menu = TerminalMenu(
+        ["View Passwords", "Create New Entry", "Update Password", "Logout"]
+    )
+    while True:
+        user_selection = user_menu.show()
+        if user_selection == 3:
+            print("Logging out...")
+            print(f"User {user.username} has successfully logged out.")
+            break
 
 
 def main():
@@ -37,4 +60,20 @@ def main():
 
 
 if __name__ == "__main__":
+    print(
+        """
+=======================================================================================================================================
+=======================================================================================================================================
+
+██████╗  █████╗ ███████╗███████╗██╗    ██╗ ██████╗ ██████╗ ██████╗     ███╗   ███╗ █████╗ ███╗   ██╗ █████╗  ██████╗ ███████╗██████╗ 
+██╔══██╗██╔══██╗██╔════╝██╔════╝██║    ██║██╔═══██╗██╔══██╗██╔══██╗    ████╗ ████║██╔══██╗████╗  ██║██╔══██╗██╔════╝ ██╔════╝██╔══██╗
+██████╔╝███████║███████╗███████╗██║ █╗ ██║██║   ██║██████╔╝██║  ██║    ██╔████╔██║███████║██╔██╗ ██║███████║██║  ███╗█████╗  ██████╔╝
+██╔═══╝ ██╔══██║╚════██║╚════██║██║███╗██║██║   ██║██╔══██╗██║  ██║    ██║╚██╔╝██║██╔══██║██║╚██╗██║██╔══██║██║   ██║██╔══╝  ██╔══██╗
+██║     ██║  ██║███████║███████║╚███╔███╔╝╚██████╔╝██║  ██║██████╔╝    ██║ ╚═╝ ██║██║  ██║██║ ╚████║██║  ██║╚██████╔╝███████╗██║  ██║
+╚═╝     ╚═╝  ╚═╝╚══════╝╚══════╝ ╚══╝╚══╝  ╚═════╝ ╚═╝  ╚═╝╚═════╝     ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝
+=======================================================================================================================================
+=======================================================================================================================================
+    """
+    )
+
     main()

c7e0c2a005288732ee2c80034e2c537f275e795d
Jimmy Li <jmyli562@gmail.com>
2023-08-18 16:08:39 -0700

    add generate_password method

--- lib/cli.py
+++ lib/cli.py
@@ -2,6 +2,8 @@ from sqlalchemy.orm import sessionmaker, make_transient
 from models import User, Category, Entry, Tag, engine
 from simple_term_menu import TerminalMenu
 from datetime import datetime
+import string
+import random
 
 # Create the session
 Session = sessionmaker(bind=engine)
@@ -37,13 +39,15 @@ def add_user():
 
 def view_passwords(user):
     # retrieving all of the entries that match the filter
-    user_entry = session.query(Entry).filter_by(user_id=user.id).all()
+    user_entry = session.query(Entry).filter_by(user=user).all()
 
     # no saved passwords were found
     if not user_entry:
         print(f"User {user.username} has no saved passwords.")
         return
+
     print("\nSaved Passwords:")
+    print("-" * 30)
     for index, entry in enumerate(user_entry, start=1):
         print(f"\nEntry {index}:")
         print(f"Website: {entry.website}")
@@ -59,7 +63,69 @@ def create_entry(user):
     username = input("Please enter your username for that website: ")
     password = input("Please enter the password: ")
     note = input("Please type out a note for this website: ")
-    category = input("Please enter the category name: ")
+    name = input("Please enter the category name: ")
+
+    # Checking if the category already exists for the logged in user
+    category = session.query(Category).filter_by(name=name, user=user)
+    # If is does not exist create a new category entry in database
+    if not category:
+        print("Category not found. Creating new category...")
+        category = Category(name=name, user=user)
+        session.add(category)
+        session.commit()
+
+    current_time = datetime.now()  # Get the current timestamp
+
+    new_entry = Entry(
+        website=website,
+        username=username,
+        password=password,
+        notes=note,
+        created_at=current_time,  # Set the created_at timestamp
+        updated_at=None,  # Set the updated_at initially to NULL
+        user=user,
+        category=category,
+    )
+    session.add(new_entry)
+    session.commit()
+    print("New Entry has been created successfully!")
+
+
+def generate_password():
+    # Prompt user for password length, making sure that the input is an integer
+    length = int(input("Enter desired password length: "))
+
+    # Prompt user for including special characters
+    include_special = input("Include special characters? (yn): ")
+
+    ascii = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    digits = "0123456789"
+    characters = ascii + digits
+    if include_special == "Y" or include_special == "y":
+        punctuation = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~."
+        characters += punctuation
+
+    # Generate and return password using random choice
+    password = "".join(random.choice(characters) for i in range(length))
+    return password
+
+
+def update_password(user):
+    website = input("Enter website for the password you want to update: ")
+    prompt = input("Would you like to generate a secure password? [y/n]?")
+    if prompt == "Y":
+        new_password = generate_password()
+    else:
+        new_password = input("Please enter the new password: ")
+
+    entry = session.query(Entry).filter_by(website=website, user=user).first()
+    if entry:
+        entry.password = new_password
+        entry.updated_at = datetime.now()  # Update the updated_at timestamp
+        session.commit()
+        print("Password updated successfully!")
+    else:
+        print("Entry was not found. Password update failed.")
 
 
 def user_menu(user):
@@ -81,7 +147,7 @@ def user_menu(user):
         elif user_selection == 1:
             create_entry(user)
         elif user_selection == 2:
-            pass
+            update_password(user)
         elif user_selection == 3:
             pass
         elif user_selection == 4:
